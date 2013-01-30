b16715c5863b4d65aed23ef3d46c1c1898faf399
William Myers <griffin.myers@gmail.com>
2013-01-30 03:05:42 -0500

    Build out HTML methods for itemize picking

--- lib/hemingway/block/block.rb
+++ lib/hemingway/block/block.rb
@@ -1,7 +1,20 @@
 module Hemingway
   module ItemizeBlockNode
     def html
-      ""
+      Build.tag("ul", itemize_content.elements.map { |e| e.html }.join)
+    end
+  end
+
+  module ItemNode
+    def html
+      label_tag = label.empty? ? "" : Build.tag("span", label.html, "label")
+      Build.tag("li", label_tag + sequence.elements.map { |e| e.html }.join)
+    end
+  end
+
+  module LabelNode
+    def html
+      label_content.html
     end
   end
 end
