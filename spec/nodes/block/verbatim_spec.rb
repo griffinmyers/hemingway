85fefcd9af5ecc1a30251aefce5eef97a24506e9
William Myers <griffin.myers@gmail.com>
2013-02-24 13:06:48 -0500

    Fixed verbatim so you can put tex markup inside a verbatim block

--- spec/nodes/block/verbatim_spec.rb
+++ spec/nodes/block/verbatim_spec.rb
@@ -6,13 +6,18 @@ module Hemingway
 
     before do
       @parser = Parser.new
-    end 
+    end
 
     describe "#verbatim" do
 
       it 'allows me to instantiate pre-formatted text' do
         html = @parser.parse("\\begin{verbatim} don't  we  \n \n all wish \n \n   for a little sleep? \\end{verbatim}").html
         html.should == "<div class='entry'><p><pre>don't  we  \n \n all wish \n \n   for a little sleep? </pre></p></div>"
+      end
+
+      it 'allows me to write tex markup that isnt parses' do
+        html = @parser.parse("\\begin{verbatim} Emphasized text uses the \\emph{} tag. \\end{verbatim}").html
+        html.should == "<div class='entry'><p><pre>Emphasized text uses the \\emph{} tag. </pre></p></div>"
       end
 
     end
