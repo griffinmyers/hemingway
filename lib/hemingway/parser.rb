128d3ff1d14fb83468982c228de9ba15e08c5df5
William Myers <griffin.myers@gmail.com>
2013-02-25 00:51:20 -0500

    Updated non-text to be less redundant.

--- lib/hemingway/parser.rb
+++ lib/hemingway/parser.rb
@@ -4,7 +4,7 @@ require "hemingway/latex.treetop"
 
 module Hemingway
 
-  # This class is a thin wrapper around the LatexParser built by Treetop
+  # This class is a thin wrapper around the LatexParser built by Treetop.
   # At first I used this to pad input, but as it currently stands it serves
   # no purpose. I'm leaving the infrastructure in-tact for the moment
   # incase it occurs to me that some method would be nice to have on it.
