a5a941dbb46fd41bcd71a1f558352e2b4f0e1fef
William Myers <griffin.myers@gmail.com>
2013-08-09 20:00:55 -0400

    open links in new window

--- lib/hemingway/link/link_nodes.rb
+++ lib/hemingway/link/link_nodes.rb
@@ -1,7 +1,7 @@
 module Hemingway
   module LinkNode
     def html
-      Build.tag("a", description.text_value, :href => url.text_value)
+      Build.tag("a", description.text_value, :href => url.text_value, :target => "_blank")
     end
   end
 end
--- spec/nodes/link_spec.rb
+++ spec/nodes/link_spec.rb
@@ -12,7 +12,7 @@ module Hemingway
 
       it "parse ling tags" do
         html = @parser.parse("\\href{http://yachtvibe.com}{Click 4 Vibes}").html
-        html.should == "<div class='entry'><p><a href='http://yachtvibe.com'>Click 4 Vibes</a></p></div>"
+        html.should == "<div class='entry'><p><a href='http://yachtvibe.com' target='_blank'>Click 4 Vibes</a></p></div>"
       end
 
     end
